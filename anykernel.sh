d620d5e7be6d7ffb1d6c8a2ab8a79e275f0261b0
K4pren3l <gabriel.jesus.main23@gmail.com>
2026-05-02 12:12:09 -0300

    resukisu + susfs 2.0.1 android13-5.10.223

---
+++ anykernel.sh
@@ -0,0 +1,30 @@
+properties() { '
+kernel.string=ReSukiSU Android13 5.10
+do.devicecheck=0
+do.modules=0
+do.systemless=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
+'; }
+
+block=auto
+is_slot_device=auto
+ramdisk_compression=auto
+patch_vbmeta_flag=auto
+no_magisk_check=1
+
+. tools/ak3-core.sh
+
+split_boot
+if [ -f "split_img/ramdisk.cpio" ]; then
+    unpack_ramdisk
+    write_boot
+else
+    flash_boot
+fi
